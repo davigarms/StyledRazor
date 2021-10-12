638435cebd780ad797411c8f51cf55960286e348
Davi Garms <davigarms@gmail.com>
2021-10-12 09:27:18 +0100

    Initialise Blazor and introduce some layout components

---
+++ Shared/Wrapper.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BlazorApp.Shared
+{
+    public class Wrapper
+    {
+        public static string UseId() => $"wrapper_{Guid.NewGuid().ToString()[..8]}";
+    }
+}
---
+++ Styles/StyleDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlazorApp.Styles
+{
+    public static class StyleDictionary
+    {
+        public static String SpacingS => "10px";
+        public static String SpacingM => "30px";
+        public static String SpacingL => "50px";
+    }
+}
