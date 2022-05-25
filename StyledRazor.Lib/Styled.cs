3cb34a3b844bab477988b2b35f7807b4e59a52a1
Davi Garms <davigarms@gmail.com>
2022-05-25 20:24:08 +0100

    Refactor Styled class and organise Lib folder

--- StyledRazor.Lib/Styled.cs
+++ StyledRazor.Lib/Styled.cs
@@ -5,85 +5,91 @@ using Microsoft.AspNetCore.Components.Rendering;
 
 namespace StyledRazor.Lib
 {
-    public class Styled 
+    public class Styled
     {
         private readonly string _componentId;
-        private IReadOnlyDictionary<string,object> _params;
+        private IReadOnlyDictionary<string, object> _params;
         private RenderFragment _childContent;
         private string _css;
         private string _baseElement = "div";
+        public string Type { get; }
 
-        public Styled() => _componentId = GenerateId();
-        
-        public Styled(string css) : this () => SetCss(css);
 
-        public Styled(Type type) => _componentId = GenerateId(type.Name);
-        
-        public Styled(string css, Type type) : this (type) => SetCss(css);
-
-        public Styled(ComponentBase componentBase) => _componentId = GenerateId(componentBase.GetType().Name);
-        
-        public Styled(string css, ComponentBase componentBase) : this (componentBase) => SetCss(css);
-
-        private string GenerateId(string prefix = null) =>
-            $"{(prefix == null ? "w" : prefix.ToLower() + "-w")}{Guid.NewGuid().ToString()[..8]}";
-        
-        private static string BaseElement(string css) => css.Substring(0, css.IndexOf("{")).Trim();
-        
-        private void SetCss(string css)
+        public Styled(string css = null, string type = null)
         {
-            _css = css;
-            _baseElement = BaseElement(css);
+            Type = type;
+            _componentId = GenerateId(type);
+            SetCss(css);
         }
 
-        public Styled Params(IReadOnlyDictionary<string,object> @params)
+        public Styled(string css, Type type)
+            : this(css, type.Name) => SetCss(css);
+
+        public Styled(string css, ComponentBase component) :
+            this(css, component.GetType().Name) => SetCss(css);
+
+        private static string GenerateId(string prefix = null) =>
+            $"{(prefix == null ? "w" : prefix.ToLower() + "-w")}{Guid.NewGuid().ToString()[..8]}";
+
+        private static string BaseElement(string css) => css.Substring(0, css.IndexOf("{")).Trim();
+
+        private void SetCss(string css)
+        {
+            if (css == null) return;
+            _baseElement = BaseElement(css);
+            _css = css
+                .Insert(0, "\n")
+                .Replace("}", "}\n")
+                .Replace("  ", "")
+                .Replace("\r", "\n")
+                .Replace("\n ", "\n")
+                .Replace("\t", "")
+                .Replace("\n" + _baseElement, $"{_baseElement}[{_componentId}]")
+                .Replace("\n", "");
+        }
+
+        public Styled Params(IReadOnlyDictionary<string, object> @params)
         {
             _params = @params;
             return this;
         }
-        
+
         public Styled Content(RenderFragment childContent)
         {
             _childContent = childContent;
             return this;
         }
 
-        public Styled Css(string css)
-        {
-            SetCss(css);
-            return this;
-        }
-       
         public RenderFragment Render() => component =>
         {
             component.OpenElement(0, _baseElement);
-            ComponentId(component);
-            ComponentParams(component);
-            ComponentContent(component);
-            ComponentCss(component);
+            BuildComponentId(component);
+            BuildComponentParams(component);
+            BuildComponentContent(component);
+            BuildComponentCss(component);
             component.CloseElement();
         };
 
-        private void ComponentId(RenderTreeBuilder builder) => builder.AddAttribute(0, _componentId);
+        private void BuildComponentId(RenderTreeBuilder builder) => builder.AddAttribute(0, _componentId);
 
-        private void ComponentParams(RenderTreeBuilder builder)
+        private void BuildComponentParams(RenderTreeBuilder builder)
         {
             if (_params == null) return;
             foreach (var (key, value) in _params)
                 builder.AddAttribute(0, key, value);
         }
 
-        private void ComponentContent(RenderTreeBuilder builder)
+        private void BuildComponentContent(RenderTreeBuilder builder)
         {
-            if (_childContent == null) return; 
+            if (_childContent == null) return;
             builder.AddContent(0, _childContent);
         }
 
-        private void ComponentCss(RenderTreeBuilder builder)
+        private void BuildComponentCss(RenderTreeBuilder builder)
         {
             if (_css == null) return;
             builder.OpenElement(0, "style");
-            builder.AddContent(0, _css.Replace(_baseElement, $"{_baseElement}[{_componentId}]"));
+            builder.AddContent(0, _css);
             builder.CloseElement();
         }
     }
