48a724e36b5fc4796b75f793a492cd38777190e8
Davi Garms <davigarms@gmail.com>
2024-01-16 23:18:54 +0000

    Reorganise CssDeclarationDictionary tests

--- StyledRazor.Core.UnitTests/CssDeclarationDictionaryShould.cs
+++ StyledRazor.Core.UnitTests/CssDeclarationDictionaryShould.cs
@@ -5,47 +5,63 @@ namespace StyledRazor.Core.UnitTests;
 [TestFixture]
 public class CssDeclarationDictionaryShould
 {
-  private const string Selector = "> *";
-  private readonly CssDeclarationDictionary _expectedDeclaration = new()
+  private const string Property = "Property";
+  private const string InitialValue = "InitialValue";
+  private const string NewValue = "NewValue";
+  private static readonly string Selector = TestContext.CurrentContext.Random.GetString(4);
+
+  private static readonly CssDeclarationDictionary InitialDeclaration = new()
   {
-    { "margin", "10px" },
+    [Property] = InitialValue,
   };
   
-  private CssRulesetDictionary _css;
+  private static readonly CssDeclarationDictionary NewDeclaration = new()
+  {
+    [Property] = NewValue,
+  };
+  
+  private static CssRulesetDictionary _css = new();
 
   [SetUp]
-  public void SetUp()
+  public void SetUp() => _css = new CssRulesetDictionary();
+  
+  [Test]
+  public void SetNewProperty_WhenPropertyIsNotDefined()
   {
-    _css = new CssRulesetDictionary();
-  }
-
-  [TestCase("0")]
-  [TestCase("10px")]
-  public void SetProperty_WhenPropertyIsAlreadyDefined(string marginValue)
-  {
-    _css[Selector] = new CssDeclarationDictionary
-    {
-      { "margin", marginValue },
-    };
-
-    _css.Get(Selector).Set("margin", "10px");
+    _css.Get(Selector).Set(Property, NewValue);
 
     Assert.Multiple(() =>
     {
       Assert.That(_css.Keys, Does.Contain(Selector));
-      Assert.That(_css[Selector], Is.EquivalentTo(_expectedDeclaration));
+      Assert.That(_css[Selector], Is.EqualTo(NewDeclaration));
     });
   }
 
   [Test]
-  public void SetNewProperty_WhenPropertyIsNotDefined()
+  public void SetProperty_WhenPropertyIsAlreadyDefined()
   {
-    _css.Get(Selector).Set("margin", "10px");
+    _css[Selector] = InitialDeclaration;
+
+    _css.Get(Selector).Set(Property, NewValue);
 
     Assert.Multiple(() =>
     {
       Assert.That(_css.Keys, Does.Contain(Selector));
-      Assert.That(_css[Selector], Is.EquivalentTo(_expectedDeclaration));
+      Assert.That(_css[Selector], Is.EqualTo(NewDeclaration));
+    });
+  }
+
+  [Test]
+  public void NotSetProperty_WhenValueIsAlreadySetForProperty()
+  {
+    _css[Selector] = InitialDeclaration;
+      
+    _css.Get(Selector).Set(Property, InitialValue);
+
+    Assert.Multiple(() =>
+    {
+      Assert.That(_css.Keys, Does.Contain(Selector));
+      Assert.That(_css[Selector], Is.EqualTo(InitialDeclaration));
     });
   }
 }
