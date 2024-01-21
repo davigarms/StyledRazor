1d48ca98a86d13f262ccfef45348ea0cfb07c824
Davi Garms <davigarms@gmail.com>
2024-01-21 17:06:50 +0000

    Add ITokens interface

---
+++ StyledRazor.Core/Style/ITokens.cs
@@ -0,0 +1,40 @@
+namespace StyledRazor.Core.Style;
+
+public interface ITokens
+{
+  public string Zero { get; }
+  public string Auto { get; }
+  public string SpacingXs { get; }
+  public string SpacingS { get; }
+  public string SpacingM { get; }
+  public string SpacingL { get; }
+  public string SpacingXl { get; }
+  public string SpacingXxl { get; }
+  public string Initial { get; }
+  public string SizeViewPortWidth { get; }
+  public string SizeViewportHeight { get; }
+  public string SizeTotal { get; }
+  public string AlignLeft { get; }
+  public string AlignCenter { get; }
+  public string AlignRight { get; }
+  public string AlignJustify { get; }
+  public string AlignSpaceBetween { get; }
+  public string AlignTop { get; }
+  public string AlignEnd { get; }
+  public string AlignFlexStart { get; }
+  public string AlignFlexEnd { get; }
+  public string DisplayInlineFlex { get; }
+  public string DisplayFlex { get; }
+  public string DisplayBlock { get; }
+  public string DirectionColumn { get; }
+  public string DirectionRow { get; }
+  public string FlexWrap { get; }
+  public string FlexWrapReverse { get; }
+  public string FlexNoWrap { get; }
+  public string ScreenXs { get; }
+  public string ScreenS { get; }
+  public string ScreenM { get; }
+  public string ScreenLg { get; }
+  public string ScreenXlg { get; }
+  public string ScreenXxg { get; }
+}
