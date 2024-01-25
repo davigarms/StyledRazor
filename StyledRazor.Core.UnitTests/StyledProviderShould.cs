cafc4c46832011e8e706c27d4f0ae7c53e32d7b9
Davi Garms <davigarms@gmail.com>
2024-01-25 22:26:40 +0000

    Change StyledProvider tests

--- StyledRazor.Core.UnitTests/StyledProviderShould.cs
+++ StyledRazor.Core.UnitTests/StyledProviderShould.cs
@@ -14,20 +14,21 @@ public class StyledProviderShould
   }
 
   [Test]
-  public void GetStyled_FromAStyledBaseComponent()
+  public void GetStyled_FromAStyledComponent()
   {
     var tokens = new Tokens();
     var styledProvider = new StyledProvider(tokens);
+    const string expectedCssString = "div[TestComponent]{Property:Value;}";
+    
     var styled = styledProvider.Get(typeof(TestComponent));
-    var expected = new TestComponent(tokens).Base;
 
     Assert.Multiple(() =>
     {
-      Assert.That(styled.Type, Is.EqualTo(expected.Type));
-      Assert.That(styled.CssString, Is.EqualTo(ExpectedCssStringWithScopeFrom(styled, expected)));
+      Assert.That(styled.Type, Is.EqualTo(typeof(TestComponent)));
+      Assert.That(styled.CssString, Is.EqualTo(ExpectedCssStringWithScopeFrom(styled, expectedCssString)));
     });
   }
   
-  private static string ExpectedCssStringWithScopeFrom(Styled styled, Styled expectedStyled) => 
-    expectedStyled.CssString.Replace(expectedStyled.Id, styled.Id);
+  private static string ExpectedCssStringWithScopeFrom(Styled styled, string expected) => 
+    expected.Replace("TestComponent", styled.Id);
 }
