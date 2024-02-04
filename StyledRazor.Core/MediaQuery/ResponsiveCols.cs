7d281e7038643392d6074887e311a045b2dd95a9
Davi Garms <davigarms@gmail.com>
2024-02-04 21:34:39 +0000

    Refactor media query implementation

--- StyledRazor.Core/MediaQuery/ResponsiveCols.cs
+++ StyledRazor.Core/MediaQuery/ResponsiveCols.cs
@@ -1,15 +1,41 @@
-using System.Collections.Generic;
+using System.Linq;
 
 namespace StyledRazor.Core.MediaQuery;
 
-public class ResponsiveCols : Dictionary<BreakPoint, int> { }
-
-public enum BreakPoint
+public class ResponsiveCols
 {
-  Xs = 0,
-  Sm = 480,
-  Md = 768,
-  Lg = 960,
-  Xl = 1280,
-  Xxl = 1536,
+  private readonly MediaQuery _mediaQuery = new();
+
+  public ResponsiveCols(int xs = 0, int sm = 0, int md = 0, int lg = 0, int xl = 0, int xxl = 0)
+  {
+    if (xs > 0) _mediaQuery.Add(BreakPoint.Xs, xs);
+    if (sm > 0) _mediaQuery.Add(BreakPoint.Sm, sm);
+    if (md > 0) _mediaQuery.Add(BreakPoint.Md, md);
+    if (lg > 0) _mediaQuery.Add(BreakPoint.Lg, lg);
+    if (xl > 0) _mediaQuery.Add(BreakPoint.Xl, xl);
+    if (xxl > 0) _mediaQuery.Add(BreakPoint.Xxl, xxl);
+  }
+
+  public static ResponsiveCols Set(int xs = 0, int sm = 0, int md = 0, int lg = 0, int xl = 0, int xxl = 0)
+    => new(xs, sm, md, lg, xl, xxl);
+
+  public int? NumberOfColumnsFor(int windowWidth)
+  {
+    if (_mediaQuery.Count == 0) return null;
+
+    for (var i = 0; i < _mediaQuery.Count - 1; i++)
+    {
+      var minQuery = _mediaQuery.ElementAt(i);
+      var maxQuery = _mediaQuery.ElementAt(i + 1);
+
+      if (windowWidth < (int)minQuery.Key ||
+          (windowWidth >= (int)minQuery.Key && windowWidth < (int)maxQuery.Key))
+      {
+        return minQuery.Value;
+      }
+    }
+
+    var lastQuery = _mediaQuery.Last();
+    return windowWidth > (int)lastQuery.Key ? lastQuery.Value : null;
+  }
 }
--- StyledRazor.Web/Startup.cs
+++ StyledRazor.Web/Startup.cs
@@ -29,7 +29,6 @@ namespace StyledRazor.Web
             services.AddScoped<StyledProvider>();
             services.AddScoped<ITokens, Tokens>();
             services.AddScoped<BrowserService>();
-            services.AddScoped<MediaQueryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
