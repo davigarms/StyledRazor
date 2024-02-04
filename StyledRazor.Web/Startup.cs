457bf63c483320c2acfea1d17e6c8f9ffc481d09
Davi Garms <davigarms@gmail.com>
2024-02-04 21:34:39 +0000

    Refactor media query implementation

--- StyledRazor.Web/Startup.cs
+++ StyledRazor.Web/Startup.cs
@@ -30,7 +30,6 @@ namespace StyledRazor.Web
             services.AddScoped<StyledProvider>();
             services.AddScoped<ITokens, Tokens>();
             services.AddScoped<BrowserService>();
-            services.AddScoped<MediaQueryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
