9a4bdf80cb5c3bbba468a869d768d7c461193d92
Davi Garms <davigarms@gmail.com>
2024-02-10 13:09:09 +0000

    Make ColumnsByBreakpoint private

--- StyledRazor.Core/MediaQuery/ColumnsService.cs
+++ StyledRazor.Core/MediaQuery/ColumnsService.cs
@@ -8,7 +8,7 @@ public class ColumnsService
 {
   private readonly ITokens _tokens;
 
-  public Dictionary<int, int> ColumnsByBreakpoint { get; private set; } = new();
+  private Dictionary<int, int> ColumnsByBreakpoint { get; set; } = new();
 
   public ColumnsService(ITokens tokens)
   {
